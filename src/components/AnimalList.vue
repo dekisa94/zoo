e5dbf9797b906b0da3d1b0ef67169242a8dc58d9
dekisa94 <dsimic94@gmail.com>
2018-04-24 18:29:43 +0200

    first commit

---
+++ src/components/AnimalList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <table>
+      <thead>
+      <th>Type</th>
+      <th>Name</th>
+      <th>Date of birth</th>
+      </thead>
+      <tbody>
+        <tr v-for= "(animal,key) in animals" :key="key">
+        <td> {{animal.type}} </td>
+        <td> {{animal.name}} </td>
+        <td> {{animal.dateOfBirth}} </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AnimalList',
+  data(){
+    return{
+      animals:[
+        {type: 'Girrafe', name: 'Doe', dateOfBirth: '2017-03-05'},
+        {type: 'Tiger', name: 'Johan', dateOfBirth: '2013-07-05'},
+        {type: 'Wolf', name: 'Daglas', dateOfBirth: '2003-08-11'},
+        {type: 'Monkey', name: 'Sead', dateOfBirth: '2018-04-02'},
+        {type: 'Elephant', name: 'Joe', dateOfBirth: '1998-06-05'}
+      ]
+    }
+  }
+}
+</script>
